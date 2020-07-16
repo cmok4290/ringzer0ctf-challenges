e8d4d9e7ac5d2decd0aae19f3d5668de1d3ea3a1
Chheany Mok <cmok4290@gmail.com>
2020-07-15 20:06:52 -0400

    Refactor challenge solutions

--- coding_challenges/13.py
+++ coding_challenges/13.py
@@ -1,44 +1,19 @@
 #!/usr/bin/env python
-from bs4 import BeautifulSoup
-import hashlib
-import json
-import re
-import requests
+from lib.challenge import Challenge
 import time
 
 
 def main():
-    # initial config
-    s = requests.Session()
-    url = 'https://ringzer0ctf.com/challenges/13'
-
-    with open('config.json', 'r') as f:
-        config = json.load(f)
-
-    cookie = {"PHPSESSID": config["PHPSESSID"]}
-    regex = re.compile(r'[\n\r\t]')
-
-    # initial request
-    r = s.get(url, cookies=cookie)
     start = time.time()
-    soup = BeautifulSoup(r.text, features="lxml")
 
-    # retrieve message
-    tag = soup.find("div", class_="message")
-    message = tag.contents[2]
-    message = regex.sub("", message)
-
-    # create hash
-    hashed = hashlib.sha512(message.encode()).hexdigest()
-
-    # submit hash
-    r = s.get(f'{url}/{hashed}', cookies=cookie)
-    soup = BeautifulSoup(r.text, features="lxml")
-
-    # retrieve challenge flag
-    tag = soup.find("div", class_="alert alert-info")
-    flag = tag.contents[0]
+    challenge = Challenge(13)
+    message = challenge.get_challenge()
+    print(message)
+    hashed_message = challenge.get_hash("sha512", message)
+    print(hashed_message)
+    flag = challenge.post_challenge(hashed_message)
     print(flag)
+
     end = time.time() - start
     print(round(end, 3), 's')
 
--- coding_challenges/14.py
+++ coding_challenges/14.py
@@ -1,48 +1,21 @@
 #!/usr/bin/env python
-from bs4 import BeautifulSoup
-import hashlib
-import json
-import re
-import requests
+from lib.challenge import Challenge
 import time
 
 
 def main():
-    # initial config
-    s = requests.Session()
-    url = 'https://ringzer0ctf.com/challenges/14'
-
-    with open('config.json', 'r') as f:
-        config = json.load(f)
-
-    cookie = {"PHPSESSID": config["PHPSESSID"]}
-    regex = re.compile(r'[\n\r\t]')
-
-    # initial request
-    r = s.get(url, cookies=cookie)
     start = time.time()
 
-    # retrieve message
-    soup = BeautifulSoup(r.content, features="lxml")
-    tag = soup.find("div", class_="message")
-    message = tag.contents[2]
-    message = regex.sub("", message)
-    # group bits into bytes or 8 bits
-    # convert bytes to integers then characters
+    challenge = Challenge(14)
+    message = challenge.get_challenge()
+    print(message)
     message = ''.join([chr(int(message[i:i+8], 2))
                        for i in range(0, len(message), 8)])
-
-    # create hash
-    hashed = hashlib.sha512(message.encode('utf-8')).hexdigest()
-
-    # submit hash
-    r = s.get(f'{url}/{hashed}', cookies=cookie)
-    soup = BeautifulSoup(r.content, features="lxml")
-
-    # retrieve challenge flag
-    tag = soup.find("div", class_="alert alert-info")
-    flag = tag.contents[0]
+    hashed_message = challenge.get_hash("sha512", message)
+    print(hashed_message)
+    flag = challenge.post_challenge(hashed_message)
     print(flag)
+
     end = time.time() - start
     print(round(end, 3), 's')
 
