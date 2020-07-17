31a77aa8fe92f7e872df685d0c1b51ac3b0005c9
Chheany Mok <cmok4290@gmail.com>
2020-07-16 21:09:19 -0400

    Refactor a bit

--- coding_challenges/56.py
+++ coding_challenges/56.py
@@ -1,14 +1,36 @@
 #!/usr/bin/env python
-from lib.challenge import Challenge
-from lib.hash_decryptor import Decryptor
+from utils.constants import URL
+from utils.hash_decryptor import Decryptor
+import utils.challenge as c
+import requests
+import time
+import sys
+
 
 def main():
-    c = Challenge(56)
-    msg = c.get_challenge()
-    d = Decryptor(msg)
-    decrypted = d.decrypt()
-    flag = c.post_challenge(decrypted)
+    prog_start = time.time()
+    challenge_id = sys.argv[0].split('.')[0]
+
+    session = requests.Session()
+    cookie = c.get_cookie('config.json')
+
+    challenge, messages = c.get_challenge(session, URL, challenge_id, cookie)
+    # print(challenge)
+    # print(messages)
+
+    cha_start = time.time()
+    seconds = c.get_seconds(challenge.contents[1])
+    payload = Decryptor(messages[0]).decrypt()
+    flag = c.post_challenge(session, URL, challenge_id, cookie, payload)
     print(flag)
+
+    cha_end = time.time() - cha_start
+    print(f'challenge: {seconds}s')
+    print(f'actual: {round(cha_end, 3)}s')
+
+    prog_end = time.time() - prog_start
+    print(f'total: {round(prog_end, 3)}s')
+
 
 if __name__ == '__main__':
     main()
