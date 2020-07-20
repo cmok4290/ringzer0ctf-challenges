cb365725b6e59eaa325d118799e59f62eb98f112
Chheany Mok <cmok4290@gmail.com>
2020-07-20 15:29:02 -0400

    Remove utils

--- coding_challenges/utils/constants.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-from urllib.request import urlopen
-
-COMMON_PASSWORDS = str(urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt').read(), 'utf-8').split('\n')
-CHALLENGES = 'https://ringzer0ctf.com/challenges'
-LOGIN = 'https://ringzer0ctf.com/login'
