3560d264541558e90018bc10b3fbe2f94220143b
Chheany Mok <cmok4290@gmail.com>
2020-09-04 13:58:03 -0400

    Remove tests

--- tests/test_hash_breaker.py
+++
@@ -1,8 +0,0 @@
-# tests/test_hash_breaker.py
-from coding_challenges import hash_breaker
-
-
-def test_hash_breaker():
-    flag = hash_breaker.main()
-    assert isinstance(flag, str)
-    assert flag.startswith('FLAG')
